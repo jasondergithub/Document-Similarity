7c10716ab3f115d083734682c831a5948526140d
Jason Liu <j84yes116@gmail.com>
2021-12-06 17:52:46 +0800

    increase threshold when training

--- main/stage1/train.py
+++ main/stage1/train.py
@@ -58,7 +58,7 @@ def run_train(tableNumber):
         outputs, targets, loss = engine.train_fn(train_data_loader, model, optimizer, device, scheduler)
         print(f'Epoch:{epoch+1}, Loss:{loss:.4f}')
 
-        outputs = np.array(outputs) >= 0.5
+        outputs = np.array(outputs) >= 0.8
         accuracy = metrics.accuracy_score(targets, outputs)
         print(f"After training {epoch+1} epoch(s), Accuracy Score = {accuracy}")
     #save model
