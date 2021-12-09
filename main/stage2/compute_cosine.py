4fdc6c212d6b18e46a170b8daa63fef33d677bd7
Jason Liu <j84yes116@gmail.com>
2021-12-09 13:45:49 +0800

    revise function

--- main/stage2/compute_cosine.py
+++ main/stage2/compute_cosine.py
@@ -6,10 +6,25 @@ import torch.nn as nn
 import sys
 sys.path.insert(1, '../stage1/')
 import config
+
+from itertools import combinations
 from transformers import BertForSequenceClassification
 
 def computation():
-    print(config.EPOCHS)
+    example = [(1001, 7), (1001, 10), (1001, 12), (1008, 100), (1008, 125), (1008, 130), (1200, 1), (1200, 6)]
+    firstElement = example[0][0]
+    temp = []
+    ans = []
+    for element in example:        
+        if firstElement != element[0]:
+            result = combinations(temp, 2)
+            ans += list(result)
+            firstElement = element[0]
+            temp.clear()        
+        temp.append(element[1])
+
+    ans += list(combinations(temp, 2))
+    print(ans)
 
 if __name__ == "__main__":
     computation()
