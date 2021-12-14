75c32d5cbb18a51d64e2bdf8f6e83aa488818fae
109598089 <t109598089@ntut.org.tw>
2021-12-14 14:13:48 +0800

    new preprocessing

--- preprocessing/load_table.py
+++ preprocessing/load_table.py
@@ -10,20 +10,20 @@ with open("../dict/unrelatedTable.txt", "rb") as fp:
 name_list = ['table1.txt', 'table2.txt', 'table3.txt', 'table4.txt', 'table5.txt', 'table6.txt', 
 'table7.txt', 'table8.txt', 'table9.txt', 'table10.txt', 'table11.txt', 'table12.txt', 'table13.txt', 'table14.txt',
 'table15.txt', 'table16.txt', 'table17.txt', 'table18.txt', 'table19.txt', 'table20.txt']
-for i in range(20):
-    if (i+1) % 5 == 0:
-        subtable = random.sample(unrelated_table, 2762)
-    else:
-        subtable = random.sample(unrelated_table, 1381)    
-    subtable = subtable + related_table
-    random.shuffle(subtable)
-    with open("../table/" + name_list[i], "wb") as fp:
-        pickle.dump(subtable, fp)
-
-# imbalance_name_list = ['imbalanceTable1.txt', 'imbalanceTable2.txt']
-# for i in range(2):
-#     subtable = random.sample(unrelated_table, 4143)
+# for i in range(20):
+#     if (i+1) % 5 == 0:
+#         subtable = random.sample(unrelated_table, 2762)
+#     else:
+#         subtable = random.sample(unrelated_table, 1381)    
 #     subtable = subtable + related_table
 #     random.shuffle(subtable)
-#     with open("../imbalance_table/" + imbalance_name_list[i], "wb") as fp:
+#     with open("../table/" + name_list[i], "wb") as fp:
 #         pickle.dump(subtable, fp)
+
+subtable = random.sample(unrelated_table, 6905) #8286
+for i in range(4): #4
+    subtable += related_table
+
+random.shuffle(subtable)
+with open("../table/table100.txt",  "wb") as fp:
+    pickle.dump(subtable, fp)
